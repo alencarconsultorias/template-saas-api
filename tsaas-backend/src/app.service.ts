8760b1eba4b52fd3a660bbb2a9e79bdf9b90a5cc
Alison Alencar <alisonalencar@MacBook-Pro-de-Alison.local>
2025-05-08 17:58:12 -0300

    feat(TSAAS-62): test migration Prisma #code-review

--- tsaas-backend/src/app.service.ts
+++ tsaas-backend/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Prisma } from 'generated/prisma';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+}
+
+// Service for Prisma Client
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({ data });
+  }
+
+  async findAll() {
+    return this.prisma.user.findMany();
   }
 }
